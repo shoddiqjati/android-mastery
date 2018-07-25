99cd1948f009ef428873ebcd268418e2c139243c
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2018-07-26 00:14:27 +0700

    [jati] add main layout

--- Layout/app/src/main/java/com/jati/dev/layout/MainActivity.kt
+++ Layout/app/src/main/java/com/jati/dev/layout/MainActivity.kt
@@ -2,11 +2,20 @@ package com.jati.dev.layout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
