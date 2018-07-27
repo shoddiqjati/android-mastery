ba95b6c1dfb651214cb8e5714096b16bc5c5a259
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2018-07-27 16:12:43 +0700

    [jati] add bottom navigation

--- Layout/app/src/main/java/com/jati/dev/layout/MainActivity.kt
+++ Layout/app/src/main/java/com/jati/dev/layout/MainActivity.kt
@@ -1,21 +1,73 @@
 package com.jati.dev.layout
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import kotlinx.android.synthetic.main.toolbar.*
+import android.support.v4.app.Fragment
+import com.jati.dev.layout.section.HistoryFragment
+import com.jati.dev.layout.section.MyWalletFragment
+import com.jati.dev.layout.section.OrderFragment
+import com.jati.dev.layout.section.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.design.widget.BottomNavigationView
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        initUiComponents()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
+    private fun initUiComponents() {
+        bottom_nav.setOnNavigationItemSelectedListener(navigationSelectedListener)
+        removeShiftMode(bottom_nav)
+    }
+
+    private fun selectFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private val navigationSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when (it.itemId) {
+            R.id.nav_history -> {
+                selectFragment(HistoryFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_order -> {
+                selectFragment(OrderFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_wallet -> {
+                selectFragment(MyWalletFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_settings -> {
+                selectFragment(SettingsFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun removeShiftMode(bottomNavigationView: BottomNavigationView) {
+        val menuView: BottomNavigationMenuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+        shiftingMode.isAccessible = true
+        shiftingMode.setBoolean(menuView, false)
+        shiftingMode.isAccessible = false
+        for (i in 0 until menuView.childCount) {
+            val item = menuView.getChildAt(i) as BottomNavigationItemView
+            item.setShiftingMode(false)
+            item.setChecked(item.itemData.isChecked)
+        }
     }
 }
