b8b63923477feee473029c3145605ca2141f549b
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2018-09-26 09:54:28 +0700

    [jati] bump kotlin version

---
+++ Layout/app/src/main/java/com/jati/dev/layout/section/history/HistoryViewModel.kt
@@ -0,0 +1,14 @@
+package com.jati.dev.layout.section.history
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+/**
+ * Created by Jati on 10/08/18.
+ */
+
+class HistoryViewModel(application: Application) : AndroidViewModel(application) {
+    fun test() {
+
+    }
+}
